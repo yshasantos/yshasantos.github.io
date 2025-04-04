72b2deef15cd89e386cf83c0cd056fd42fd3e65f
santoaast <ysha.santos@mail.utoronto.ca>
2025-04-04 04:20:56 -0400

    new static main pages complete, files cleaned up

---
+++ consts.js
@@ -0,0 +1,15 @@
+const navbar = `
+`;
+
+const navbarContainer = document.getElementById("navbar");
+if (navbarContainer) {
+    navbarContainer.innerHTML = navbar;
+}
+
+const navbuttons = `
+`;
+
+const navbuttonsContainer = document.getElementById("navbuttons");
+if (navbuttonsContainer) {
+    navbuttonsContainer.innerHTML = navbuttons;
+}
